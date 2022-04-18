5be04b6afe413f230d5ad62f2e5620f28d7c7a58
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-18 18:22:39 +0530

    Components Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
-import Projects from './Components/Projects';
+import Projects1 from './Components/Projects1';
+import Projects2 from './Components/Projects2';
 import Footer from './Components/Footer';
+import Committees from './Components/Committees';
+import Newsstories from './Components/Newsstories';
+import Activities from './Components/Activities';
 import {
   BrowserRouter as Router,
   Route,
   Routes
 } from 'react-router-dom'
-import Committees from './Components/Committees';
+import Downloads from './Components/Downloads';
 
 function App() {
   return (
@@ -17,8 +21,13 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path='/' element={<Home titleChange='Home'/>} />
-          <Route exact path='/projects' element={<Projects titleChange='Projects'/>} />
-          <Route exact path='/Committees' element={<Committees titleChange='Committees' />} />
+          <Route exact path='/1yearprojects' element={<Projects1 titleChange='Projects'/>} />
+          <Route exact path='/2yearprojects' element={<Projects2 titleChange='Projects'/>} />
+          <Route exact path='/committees' element={<Committees titleChange='Committees' />} />
+          <Route exact path='/newsstories' element={<Newsstories titleChange='News Stories' />} />
+          <Route exact path='/downloads' element={<Downloads titleChange='Downloads' />} />
+          <Route exact path='/activities' element={<Activities titleChange='Activities' />} />
+          
     
         </Routes>
         <Footer />
