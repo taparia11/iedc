f53bc5b576db0c5d11be225705819a5e44a87189
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-18 18:22:53 +0530

    Completed

---
+++ src/Components/Activities.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Atable from "./Scomponents/Atable"
+import Amodal from "./Scomponents/Amodal"
+
+const Activities = () => {
+  return (
+    <>
+    <Atable />
+    <Amodal />
+    </>
+  )
+}
+
+export default Activities
