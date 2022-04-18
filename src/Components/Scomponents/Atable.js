1511eece82a8bb12620c14da7c9241f82530d6b1
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-18 18:23:36 +0530

    Completed

---
+++ src/Components/Scomponents/Atable.js
@@ -0,0 +1,522 @@
+import React from 'react'
+
+const Atable = () => {
+  return (
+   <>
+   <section id="activities">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-auto">
+            <table className="table table-responsive">
+              <thead className="thead-dark">
+                <tr>
+                  <th scope="col">Event</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Gallery</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    MarchBytes with MSPC
+                  </td>
+                  <td>
+                    14/03/2020-15/03/2020
+                  </td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=14v8t1Aey8vEfWcwxw8fk1GcEREXmAmZk" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal1"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>Startup Story with SRMKZILLA</td>
+                  <td>08/02/2020</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=16NpcGxd3qA8felC67RoZd3_Q3fGB6_6e" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal2"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>Kreator Showtime</td>
+                  <td>07/02/2020</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=1F92DVHtRfkTkSr_LLCEGLsgOyGnkza7a" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal3"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>API Development Workshop with DSC SRM</td>
+                  <td>02/02/2020</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=15u08u7hVv9snH8u3zsv15qZOo797Xupd" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal4"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>Demo Day</td>
+                  <td>01/02/2020</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=1Gv9FAYShMxDmEOx4aaZYSXlVWeYwX25X" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal5"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>Big Data Analytics Workshop with MSPC</td>
+                  <td>01/02/2020</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=15D5elRsfoCL_E4Qx61nLkMz5-hwMnN1Z" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal6"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>SIH Internal Hackathon</td>
+                  <td>19/01/2020-21/01/2020</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=1GPY8SSqkrxp5t9NMtT94VFM9pMEvrYAW" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal7"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>Triumph Talks</td>
+                  <td>11/01/2020</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=1LxpHXa1sUgVBIRKiPG-5bS_0DGn8Livm" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal8"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    POC Funding Screening Session
+                  </td>
+                  <td>
+                    09/11/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal9"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>Techtalks</td>
+                  <td>19/10/2019</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=1tX7EauHrvpGuIK76owUVN6qznVt2NQgj" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal10"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>DSC X - Support</td>
+                  <td>17/10/2020</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=1Twf3TEBWlrwFlar4YpFxI02Rey7Hd-K2" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal11"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>Innovation Day</td>
+                  <td>15/10/2020</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=16xgw_GpzbjqNGdMtBVuuVi8zfh2_nMjY" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal12"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>Google ML/AI- Supported</td>
+                  <td>12/10/2020-13/10/2020</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=10jsJ8AUbh2vD0yy1cYmrA5_a55ZHwzGQ" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal13"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>Design Bootcamp</td>
+                  <td>11/10/2019-13/10/2019</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=1N64ctZkx6Go5zSWiHskvAy6Cx_oCVmDo" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal14"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Five Day FDP on " Flexible And RF Printed Electronics"
+                  </td>
+                  <td>
+                    9/10/2019-13/10/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal15"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  
+                    <td>
+                      Tech Talk - II
+                    </td>
+                    <td>
+                      30/9/2019
+                    </td>
+                    <td>
+                      <a className="btn btn-success" data-toggle="modal"
+                      href="#Modal16"
+                        >Gallery</a
+                      >
+                    </td>
+                  </tr>
+                  <tr>
+                  <td>Tech Expo</td>
+                  <td>29/9/2019</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=1r3Qrdn7Sa3iHJ73sbZ3OIwaSUbBQuk8x" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal17"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>Entrepreneurship Bootcamp</td>
+                  <td>13/9/2019-15/9/2019</td>
+                  <td>
+                    {/*-- href="https://drive.google.com/open?id=1r3Qrdn7Sa3iHJ73sbZ3OIwaSUbBQuk8x" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal18"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>React with SRMKzilla- Supported</td>
+                  <td>8/9/2019</td>
+                  <td>
+                    {/*-- href="https://tinyurl.com/FutureOWeb" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal19"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>SRMKzilla Game Design Workshop- Supported</td>
+                  <td>8/9/2019</td>
+                  <td>
+                    {/*-- href="https://tinyurl.com/MakeYourFirstVideoGame" -*/}
+                    <a
+                      className="btn btn-success"
+                      data-toggle="modal"
+                      href="#Modal20"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    ShowTime-II
+                  </td>
+                  <td>
+                    7/9/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal21"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    101 workshop on Lathe
+                  </td>
+                  <td>
+                    7/9/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal22"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Creative Arcade
+                  </td>
+                  <td>
+                    7/9/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal23"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Gen Y Inception 4.0
+                  </td>
+                  <td>
+                    7/9/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal24"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Techmux Hackathon
+                  </td>
+                  <td>
+                    23/8/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal25"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Kingston University Bootcamp
+                  </td>
+                  <td>
+                    22/7/2019-23/7/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal26"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Smart India Hackathon
+                  </td>
+                  <td>
+                    8/7/2019-15/7/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal27"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Entrepreneurship Awareness Camp
+                  </td>
+                  <td>
+                    27/06/2019-28/06/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal28"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Triumph Talks - IV
+                  </td>
+                  <td>
+                    2204/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal29"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    ShowTime-I
+                  </td>
+                  <td>
+                    12/04/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal30"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Triumph Talks- III
+                  </td>
+                  <td>
+                    04/04/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal31"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Tech Talks -I
+                  </td>
+                  <td>
+                    02/04/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal32"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Workshop on  " Resident Mentors in Innovation and Incubation Centre"
+                  </td>
+                  <td>
+                    14/3/2019-17/03/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal33"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Triumph Talks- II
+                  </td>
+                  <td>
+                    13/03/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal34"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Triumph Talks- I
+                  </td>
+                  <td>
+                    25/02/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal35"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Entrepreneurs Community Meetup-I
+                  </td>
+                  <td>
+                    09/02/2019
+                  </td>
+                  <td>
+                    <a className="btn btn-success" data-toggle="modal"
+                    href="#Modal36"
+                      >Gallery</a
+                    >
+                  </td>
+                </tr>
+
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+   </>
+  )
+}
+
+export default Atable
